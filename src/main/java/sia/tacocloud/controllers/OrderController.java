ef8035d4521fbfcd64b45031f32452c01f517f79
shorox <skazo4nix@gmail.com>
2019-07-30 23:09:13 +0300

    Listing 2.6 A controller to present a taco order form

---
+++ src/main/java/sia/tacocloud/controllers/OrderController.java
@@ -0,0 +1,20 @@
+package sia.tacocloud.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sia.tacocloud.dto.Order;
+
+@Controller
+@RequestMapping("/orders")
+@Slf4j
+public class OrderController {
+
+  @GetMapping("/current")
+  public String orderFrom(Model model) {
+    model.addAttribute("order", new Order());
+    return "orderFrom";
+  }
+}
