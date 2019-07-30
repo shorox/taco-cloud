a7b5949442866922a69d781c073347b40ef0c7ef
shorox <skazo4nix@gmail.com>
2019-07-31 00:53:08 +0300

    Listing 2.13 Validating a POSTed Order

--- src/main/java/sia/tacocloud/controllers/OrderController.java
+++ src/main/java/sia/tacocloud/controllers/OrderController.java
@@ -3,10 +3,13 @@ package sia.tacocloud.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sia.tacocloud.dto.Order;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/orders")
@@ -14,13 +17,17 @@ import sia.tacocloud.dto.Order;
 public class OrderController {
 
   @GetMapping("/current")
-  public String orderFrom(Model model) {
+  public String orderForm(Model model) {
     model.addAttribute("order", new Order());
     return "orderForm";
   }
 
   @PostMapping
-  public String processOrder(Order order) {
+  public String processOrder(@Valid Order order, Errors errors) {
+    if (errors.hasErrors()) {
+      return "orderForm";
+    }
+
     log.info("Order submitted: {}", order);
     return "redirect:/";
   }
