16fd5038c008ec49ae6b85964633413a3b51eb20
shorox <skazo4nix@gmail.com>
2019-07-31 01:48:44 +0300

    Listing 2.15 Declaring a view controller

--- src/test/java/sia/tacocloud/controllers/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/controllers/HomeControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
   @Autowired
