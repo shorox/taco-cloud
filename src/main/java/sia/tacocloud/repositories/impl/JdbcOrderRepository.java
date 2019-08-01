8f2bc89e53b3f187e1ca1585839df7c418898603
shorox <skazo4nix@gmail.com>
2019-08-02 01:59:38 +0300

    Listing 3.14 Using SimpleJdbcInsert to insert data

--- src/main/java/sia/tacocloud/repositories/impl/JdbcOrderRepository.java
+++ src/main/java/sia/tacocloud/repositories/impl/JdbcOrderRepository.java
@@ -6,7 +6,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import sia.tacocloud.dto.Order;
+import sia.tacocloud.dto.Taco;
 import sia.tacocloud.repositories.OrderRepository;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcOrderRepository implements OrderRepository {
@@ -29,6 +35,31 @@ public class JdbcOrderRepository implements OrderRepository {
 
   @Override
   public Order save(Order order) {
-    throw new UnsupportedOperationException();
+    order.setCreatedAt(new Date());
+    long orderId = saveOrderDetails(order);
+    order.setId(orderId);
+    List<Taco> tacos = order.getTacos();
+
+    for (Taco taco : tacos) {
+      saveTacoToOrder(taco, orderId);
+    }
+
+    return order;
+  }
+
+  private long saveOrderDetails(Order order) {
+    @SuppressWarnings("unchecked")
+    Map<String, Object> values = objectMapper.convertValue(order, Map.class);
+    values.put("createdAt", order.getCreatedAt());
+
+    return orderInserter.executeAndReturnKey(values).longValue();
+  }
+
+  private void saveTacoToOrder(Taco taco, long orderId) {
+    Map<String, Object> values = new HashMap<>();
+    values.put("tacoOrder", orderId);
+    values.put("taco", taco.getId());
+
+    orderTacoInserter.execute(values);
   }
 }
