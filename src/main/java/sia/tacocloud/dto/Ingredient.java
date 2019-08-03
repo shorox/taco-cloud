579f5985b950d8165225715c1ed780f133b9a7d2
shorox <skazo4nix@gmail.com>
2019-08-03 19:13:10 +0300

    Listing 3.16 Annotating Ingredient for JPA persistence

--- src/main/java/sia/tacocloud/dto/Ingredient.java
+++ src/main/java/sia/tacocloud/dto/Ingredient.java
@@ -1,10 +1,18 @@
 package sia.tacocloud.dto;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 @Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
 
+  @Id
   private final String id;
 
   private final String name;
