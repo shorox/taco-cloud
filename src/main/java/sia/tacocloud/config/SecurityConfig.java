7ab63c0ef7616c858ce8330da3d2c407c2e25d8d
shorox <skazo4nix@gmail.com>
2019-08-04 18:15:53 +0300

    Listing 4.7 A user registration controller

--- src/main/java/sia/tacocloud/config/SecurityConfig.java
+++ src/main/java/sia/tacocloud/config/SecurityConfig.java
@@ -1,28 +1,31 @@
 package sia.tacocloud.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
-  DataSource dataSource;
+  private UserDetailsService userDetailsService;
+
+  @Bean
+  public PasswordEncoder encoder() {
+    return new StandardPasswordEncoder("53cr3t");
+  }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth
-      .jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery("select username, password, enabled from users where username = ?")
-        .authoritiesByUsernameQuery("select username, authority from user_authorities where username = ?")
-        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(encoder());
   }
 }
