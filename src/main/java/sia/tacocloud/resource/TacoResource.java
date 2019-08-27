c78557043039f1a77fecc5638560b332e1205001
shorox <skazo4nix@gmail.com>
2019-08-28 00:01:05 +0300

    Listing 6.5 A taco resource carriying domain data and a list of hyperlinks

---
+++ src/main/java/sia/tacocloud/resource/TacoResource.java
@@ -0,0 +1,25 @@
+package sia.tacocloud.resource;
+
+import lombok.Getter;
+import org.springframework.hateoas.ResourceSupport;
+import sia.tacocloud.dao.Ingredient;
+import sia.tacocloud.dao.Taco;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+public class TacoResource extends ResourceSupport {
+
+  private final String name;
+
+  private final Date createdAt;
+
+  private final List<Ingredient> ingredients;
+
+  public TacoResource(Taco taco) {
+    this.name = taco.getName();
+    this.createdAt = taco.getCreatedAt();
+    this.ingredients = taco.getIngredients();
+  }
+}
