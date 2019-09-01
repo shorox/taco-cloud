0a00768b01689950e3b790647a45f028d2597814
shorox <skazo4nix@gmail.com>
2019-09-01 03:18:51 +0300

    Listing 6.8 Adding custom links to a Spring Data REST endpoint

--- src/main/java/sia/tacocloud/config/WebConfig.java
+++ src/main/java/sia/tacocloud/config/WebConfig.java
@@ -1,8 +1,14 @@
 package sia.tacocloud.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sia.tacocloud.dao.Taco;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +17,17 @@ public class WebConfig implements WebMvcConfigurer {
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/").setViewName("home");
     registry.addViewController("/login");
+  }
+
+  @Bean
+  public ResourceProcessor<PagedResources<Resource<Taco>>> tacoProcessor(EntityLinks links) {
+    return resource -> {
+      resource.add(links.linkFor(Taco.class)
+        .slash("recent")
+        .withRel("recents")
+      );
+
+      return resource;
+    };
   }
 }
